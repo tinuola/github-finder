6a06dea2c40fd98d244abbc9fa2e75c2051783d5
tinuola <24995224+tinuola@users.noreply.github.com>
2022-01-05 15:36:57 -0800

    Reorgs src directory

---
+++ src/App-0.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <>
+        <h1>Hello Dear.</h1>
+        <h2>Goodbye Sweetie.</h2>
+        <h3>See You Later.</h3>
+      </>
+    );
+  }
+}
+
+export default App;
