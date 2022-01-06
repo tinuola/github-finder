ec869f20dd7252a887005a882ac6828a0bb6cd7c
tinuola <24995224+tinuola@users.noreply.github.com>
2022-01-05 16:11:35 -0800

    Expressions, Conditionals in JSX

--- src/App-0.js
+++ src/App-0.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  // Creating JSX expressions and conditionals
+
+  // Method as part of the class; should be referenced with "this" keyword
+  drinks = () => 'Kombucha';
+
   render(){
+    
+    const name = 'Mira Doe';
+
+    // Method declared in the render method
+    const food = () => "Lentil Dal";
+
+    // Using conditionals
+    const loading = true;
+    const showName = true;
+
     return (
       <>
-        <h1>Hello Dear.</h1>
-        <h2>Goodbye Sweetie.</h2>
-        <h3>See You Later.</h3>
+        <h2>Hello {name}.</h2>
+
+        <p>Lunch is at {2 + 3}.</p>
+        
+        {/* Methods */}
+        <p>Your favorite food is {food()} and you like to drink {this.drinks()}.</p>
+
+        {/* Conditionals */}
+        {loading ? <h2>Goodbye {name.toUpperCase()}</h2>: null}
+
+        {/* Variation on using ternary operator; uses &&, no null value */}
+        {showName && <h2>Goodbye {name.toUpperCase()}</h2>}
+        
       </>
     );
   }
