37030d2cf6c0a0a2644d71f8e1e6b387a893e51e
tinuola <24995224+tinuola@users.noreply.github.com>
2022-01-10 22:22:22 -0800

    Creates navbar component
    Introduces use of props, default props and proptypes.

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Navbar extends Component {
+  // Default props to use if no props are passed in App.js
+  static  defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+  }
+
+  // Type-checking of props
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon} /> 
+          {this.props.title}
+        </h1>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
