76d8b861a42e1adc2457f0398e66a8e10ed7b3ed
tinuola <24995224+tinuola@users.noreply.github.com>
2022-01-11 22:30:55 -0800

    Creating single user
    Working with component state
    Destructuring objects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
-
 class App extends Component {
 
   render(){
@@ -9,6 +9,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
