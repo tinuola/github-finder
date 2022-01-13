d6ed0ae45d417ac854be27d66b4fea3dae53a373
tinuola <24995224+tinuola@users.noreply.github.com>
2022-01-12 22:20:29 -0800

    Creates users wwith a list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+// import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 class App extends Component {
 
@@ -9,7 +10,8 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <Users />
+        {/* <UserItem /> */}
       </div>
     );
   }
--- src/components/users/UserItem1.js
+++ src/components/users/UserItem1.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 
 class UserItem extends Component {
 
+  // Add state in component
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo'
+  }
+
   render() {
-    const { login, avatar_url, html_url } = this.props.user;
+    // Destructuring the state values
+    const { login, avatar_url, html_url } = this.state;
 
     return (
       <div className='card text-center'>
         <img 
+        // src={this.state.avatar_url} 
         src={avatar_url} 
         alt="" 
         className="round-img" 
         style={{width: '60px'}} />
 
+        {/* <h3>{this.state.login}</h3> */}
         <h3>{login}</h3>
         <div>
           <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
